3365a4cfbef8681a1fbb8f5db832eb25178ecdef
Haranck <harankrishna03@gmail.com>
2025-12-18 09:41:49 +0530

    feat(auth): complete user registration backend

--- backend/src/infrastructure/database/mongo/repositories/UserRepository.ts
+++ backend/src/infrastructure/database/mongo/repositories/UserRepository.ts
@@ -4,10 +4,13 @@ import { UserModel } from "../models/UserModel";
 import { mapUserDocToEntity } from "../../../mappers/UserMapper";
 
 export class UserRepository implements IUserRepository {
+
     async findByEmail(email: string): Promise<UserEntity | null> {
         const doc = await UserModel.findOne({email})
+        if(!doc)return null
         return mapUserDocToEntity(doc)
     }
+
     async create(user: UserEntity): Promise<UserEntity> {
         const created = await UserModel.create(user)
         return mapUserDocToEntity(created)
